4a28e50709e9b9173c9f63ddeb216c36c51c8d0a
Gilbert <gilwong3105@gmail.com>
2020-07-31 00:16:11 -0700

    Setup initial page and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
+import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState([{ id: 1, note: 'test' }]);
   return (
-    <div className="App">
-      Note taker
+    <div className='container'>
+      <h1>Amplify Notetaker</h1>
+      <form className='mb3'>
+        <input type='text' placeholder='Write your note' />
+        <button className='pa2 f4' type='submit'>
+          Add Note
+        </button>
+      </form>
+
+      <ul>
+        {notes.map(note => {
+          return (
+            <li className='note-item' key={note.id}>
+              {note.note} <span onClick={() => console.log('hit')}>&times;</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
